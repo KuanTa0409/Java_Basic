414c81c0914091c32a29e9ad3a454c6bc154fc41
KuanTa,Lin <114749315+KuanTa0409@users.noreply.github.com>
2022-10-14 21:48:06 +0800

    Add files via upload

---
+++ whileHW.java
@@ -0,0 +1,37 @@
+package loop;
+/*
+
+*
+**
+***
+****
+*****
+
+    *
+   **
+  ***
+ ****
+*****
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+  *
+ ***
+*****
+ ***
+  *
+
+*/
+public class whileHW{
+	public static void main(String[] args) {
+	int i = 0;
+	}
+}
